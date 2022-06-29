507ff3d05b472d9124da7ce54e91567a5b9dc1d3
“Bshanz” <brianshanley24@gmail.com>
2022-06-29 11:30:05 -0400

    Sliding menu working

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const menuBtn = document.getElementById('menu-btn');
+const menuBtn = document.getElementById("menu-btn");
+let menuStatus = "";
 
-menuBtn.addEventListener("click", function(){ alert("Hello World!"); }); 
+menuBtn.addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+
+  if (menuStatus === "") {
+    menu.classList.remove("menu");
+    menu.classList.add("menu-show");
+    menuStatus = "no-show";
+    menuBtn.innerHTML = "Close Menu"
+  } else {
+    menu.classList.remove("menu-show");
+    menu.classList.add("menu");
+    menuStatus = "";
+    menuBtn.innerHTML = "Open Menu"
+  }
+});
