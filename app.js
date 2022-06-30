0f84a696ecee177e29fcf530d382f5dea37321d9
“Bshanz” <brianshanley24@gmail.com>
2022-06-30 06:57:58 -0400

    Updated if statement to use classlist.contains

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const menuBtn = document.getElementById("menu-btn");
-let menuStatus = "";
 
 menuBtn.addEventListener("click", function () {
   const menu = document.getElementById("menu");
 
-  if (menuStatus === "") {
+  if (menu.classList.contains("menu") === true) {
     menu.classList.remove("menu");
     menu.classList.add("menu-show");
-    menuStatus = "no-show";
-    menuBtn.innerHTML = "Close Menu"
+    menuBtn.innerHTML = "Close Menu";
   } else {
     menu.classList.remove("menu-show");
     menu.classList.add("menu");
-    menuStatus = "";
-    menuBtn.innerHTML = "Open Menu"
+    menuBtn.innerHTML = "Open Menu";
   }
 });
